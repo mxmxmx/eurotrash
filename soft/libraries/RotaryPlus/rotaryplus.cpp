9b6930cddedae1e7691025c81d24681db6bfc671
mxmxmx <mxxstadler@gmail.com>
2015-01-24 12:48:47 +0100

    xxx

--- soft/libraries/RotaryPlus/rotaryplus.cpp
+++ soft/libraries/RotaryPlus/rotaryplus.cpp
@@ -146,6 +146,7 @@ unsigned char Rotary::process() {
   // Return emit bits, ie the generated event.
   if ((state & 0x30) == 16) { position++; }
   if ((state & 0x30) == 32) { position--; }
+  return pinstate;
 }
 
 bool Rotary::change() {

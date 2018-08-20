e304c5914c6c4728e233d900cf4b08308d1687e9
mxmxmx <mxxstadler@gmail.com>
2018-08-20 10:26:10 +0200

    xxx

--- raspian.sh
+++ raspian.sh
@@ -1,4 +1,4 @@
-UPDATE_URL="https://raw.githubusercontent.com/mxmxmx/eurotrash/master/"
+UPDATE_URL="https://raw.githubusercontent.com/billz/raspap-webgui/master/"
 wget -q ${UPDATE_URL}/installers/common.sh -O /tmp/raspapcommon.sh
 source /tmp/raspapcommon.sh && rm -f /tmp/raspapcommon.sh
 
